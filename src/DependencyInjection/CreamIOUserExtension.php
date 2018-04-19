4857997078df3aa1674dbde41beeb8ce6d725e7d
Quentin Raimbault <quentin.raimbault@hetic.net>
2018-04-19 22:55:28 +0200

    🐛 Fix services injection

--- src/DependencyInjection/CreamIOUserExtension.php
+++ src/DependencyInjection/CreamIOUserExtension.php
@@ -2,26 +2,16 @@
 
 namespace CreamIO\UserBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class CreamIOUserExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__.'/../Resources/config')
-        );
-        $loader->load('services.yaml');
-
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $this->addAnnotatedClassesToCompile([
-            '**Bundle\\Controller\\',
-        ]);
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
